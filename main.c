c2245a0faee039c8c81b03dd767d734e5f5e9f18
Shaj2311 <haideralijafri2007@gmail.com>
2026-07-11 14:15:13 +0500

    Reset hardware to zero, load fontset and set program counter on startup

---
+++ main.c
@@ -0,0 +1,47 @@
+#include "chip8.h"
+#include <string.h>
+
+//Create VM
+Chip8 chip8;
+
+void chip8_reset_hardware();
+void chip8_init();
+
+int main()
+{
+	chip8_init();
+}
+
+void chip8_reset_hardware()
+{
+	//reset everything to zero
+
+	memset(chip8.vregs, 0, sizeof(chip8.vregs));
+	chip8.idx = 0;
+
+	memset(chip8.stack, 0, sizeof(chip8.stack));
+	chip8.sp = 0;
+
+	chip8.delay = 0;
+	chip8.sound = 0;
+
+	memset(chip8.frameBuffer, 0, sizeof(chip8.frameBuffer));
+	chip8.pc = 0;
+	memset(chip8.mem, 0, sizeof(chip8.mem));
+
+	memset(chip8.keypad, 0, sizeof(chip8.keypad));
+}
+
+void chip8_init()
+{
+	//set all hardware to zero
+	chip8_reset_hardware();
+
+	//load fontset into memory
+	memcpy(chip8.mem + 0x050, fontset, sizeof(fontset));
+
+	//TODO: load target program to memory
+
+	//set program counter to 0x200
+	chip8.pc = 0x200;
+}
