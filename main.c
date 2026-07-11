54accde1a7c115ab3389930fa1b3655d0a104a14
Shaj2311 <haideralijafri2007@gmail.com>
2026-07-11 15:33:39 +0500

    Implement reading test rom into memory

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "chip8.h"
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define DBG_ROM_NAME "test.rom"
 
 //Create VM
 Chip8 chip8;
 
 void chip8_reset_hardware();
+void chip8_load_rom(char *romName);
 void chip8_init();
 
 int main()
@@ -32,6 +36,55 @@ void chip8_reset_hardware()
 	memset(chip8.keypad, 0, sizeof(chip8.keypad));
 }
 
+void chip8_load_rom(char *romName)
+{
+	printf("Loading ROM %s\n", romName);
+
+	//open file
+	FILE *rom = fopen(romName, "rb");
+	if(rom == 0)
+	{
+		perror("fopen");
+		exit(1);
+	}
+
+	//check file size
+	if(fseek(rom, 0, SEEK_END) == -1)
+	{
+		perror("fseek");
+		exit(1);
+	}
+	long romSize = ftell(rom);
+	if(romSize == -1)
+	{
+		perror("ftell");
+		exit(1);
+	}
+
+	//exit if ROM too big
+	if(romSize > (0xFFF - 0x200))
+	{
+		puts("Error: ROM size exceeds memory");
+		exit(1);
+	}
+
+	//load ROM into memory
+	if(fread(chip8.mem + 0x200, 1, (size_t)romSize, rom) != 0)
+	{
+		perror("fread");
+		exit(1);
+	}
+
+	//close file
+	if(fclose(rom) == EOF)
+	{
+		perror("fclose");
+		exit(1);
+	}
+
+	puts("ROM loaded successfully");
+}
+
 void chip8_init()
 {
 	//set all hardware to zero
@@ -40,7 +93,8 @@ void chip8_init()
 	//load fontset into memory
 	memcpy(chip8.mem + 0x050, fontset, sizeof(fontset));
 
-	//TODO: load target program to memory
+	//load target program to memory
+	chip8_load_rom(DBG_ROM_NAME);
 
 	//set program counter to 0x200
 	chip8.pc = 0x200;
